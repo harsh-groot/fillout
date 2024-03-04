f2ab8ae9951a153e8996a01ba4ce327d83fd772d
harsh-groot <harsh.kaushal@outlook.com>
2024-03-04 10:21:02 +0530

    COMMITTED THE CODE

--- filters/index.js
+++ filters/index.js
@@ -1,24 +1,43 @@
 function applyFilters(itemsToFilter, filters, objectName = "questions") {
-
-    itemsToFilter[objectName] = itemsToFilter[objectName].filter(item =>
-        filters?.every(({ id, condition, value }) => {
-            switch (condition) {
-                case "equals":
-                    return item[id] === value;
-                case "greater_than":
-                    return item[id] > value;
-                case "less_than":
-                    return item[id] < value;
-                case "does_not_equal":
-                    return item[id] !== value;
-
-                // Add more cases for other conditions if needed
-                default:
-                    return true; // Default to true if condition not recognized
+    var list = itemsToFilter[objectName];
+    return list.filter(item => {
+        let passesAllFilters = false;
+        for (const { id, condition, value } of filters) {
+            if (item["id"] == id) {
+                switch (condition) {
+                    case "equals":
+                        if (item["value"] == value) {
+                            passesAllFilters = true;
+                        }
+                        break;
+                    case "greater_than":
+                        if (item["value"] > value) {
+                            passesAllFilters = true;
+                        }
+                        break;
+                    case "less_than":
+                        if (item["value"] < value) {
+                            passesAllFilters = true;
+                        }
+                        break;
+                    case "does_not_equal":
+                        if (item["value"] !== value) {
+                            passesAllFilters = true;
+                        }
+                        break;
+                    // Add more cases for other conditions if needed
+                }
             }
-        })
-    );
-    return itemsToFilter;
+
+            // If the item fails any condition, no need to check further, exit the loop
+            if (passesAllFilters) {
+                break;
+            }
+        }
+
+        // Return true only if the item passes all filter conditions
+        return passesAllFilters;
+    });
 }
 
 module.exports = applyFilters;
--- routes/formsRouter.js
+++ routes/formsRouter.js
@@ -1,32 +1,46 @@
 const express = require('express');
 const axios = require('axios');
 const applyFilters = require('../filters');
+const mockJson = require('../mock/mock.json');
 function routes() {
   const formsRouter = express.Router();
 
-  formsRouter.route('/forms')
+  formsRouter.route('/:formId/filteredResponses')
     .get(async (req, res) => {
-      const token = 'sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912';
+
+      const authHeader = req.headers['authorization'];
+
+      // Check if the authorization header exists
+      if (!authHeader) {
+        return res.status(401).json({ error: 'Authorization header is missing' });
+      }
+
+      // Extract the bearer token
+      const token = authHeader.split(' ')[1];
+
+      // Check if token exists
+      if (!token) {
+        return res.status(401).json({ error: 'Bearer token is missing' });
+      }
+
       const headers = {
         'Authorization': `Bearer ${token}`
       };
 
-      const response = await axios.get('https://api.fillout.com/v1/api/forms/' + req.query.formId, { headers });
-
+      //const response = await axios.get('https://api.fillout.com/v1/api/forms/' + req.params.formId, { headers });
+      const response = mockJson;
+      console.log("filteredObjects11", mockJson);
       // Extract the data from the response
-      let responseData = response.data;
-      console.log("dasdad", req.query.formId)
-      // req.filters = [
-      //   {
-      //     id: "id",
-      //     condition: "equals",
-      //     value: "4KC356y4M6W8jHPKx9QfEy",
-      //   }
-      // ];
+      //let responseData = response.data;
+      let responseData = JSON.parse(JSON.stringify(response));
 
+      //var filteredObjects = applyFilters(responseData, JSON.parse(req.query.filters) || []);
       var filteredObjects = applyFilters(responseData, JSON.parse(req.query.filters) || []);
 
-      return res.json(filteredObjects);
+      responseData.questions = filteredObjects;
+
+      return res.json(responseData);
+
     });
 
   return formsRouter;
